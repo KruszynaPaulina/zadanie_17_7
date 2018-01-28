0bc42b35bdb8e6c100f2a6d0ccc2121230332571
Paulina Kruszyna <kruszyna.paulina@gmail.com>
2018-01-28 21:27:37 +0100

    Add task 17.7

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'GOOGLE_CLIENT_ID' : '501412800649-ph61igtlmu4grpckesdu1eledsf5i6oc.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET' : 'VfTjvYiebTIwhgaYHE91OlKR',
+	'CALLBACK_URL': 'http://localhost:8500/auth/google/callback',
+};
